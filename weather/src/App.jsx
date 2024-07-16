5306906c6eb2241c70cb17358148906296be81b2
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-07-16 08:45:25 +0530

    1

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,41 +1,53 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import { CiSearch } from "react-icons/ci";
+
 
 function App() {
 
-  const [weather, setweather] = useState(null)
-  const [city, setcity] = useState("Basavakalyan")
+  const [Weather, setWeather] = useState(null)
+  const [city, setcity] = useState(null)
+
 
   const changeCity = (e) => {
     setcity(e.target.value)
   }
 
+
   useEffect(() => {
     const getWeather = async () => {
-      let apiKey = "64b71db2b9ac2e33740181245642c47a";
-      let data = 
-      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-      console.log(response)
+      let apiKey = "5f6a0548fe5be4be585f911c4df0c720";
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+      let data = await response.json()
+      console.log(data)
+      setWeather(data)
     }
     getWeather()
 
-  },)
-
-
-
-
+  }, [city])
 
 
 
   return (
     <>
       <div className=' h-[80vh] w-[80vw] bg-blue-100 '>
-        <img className='relative h-[80vh] w-[80vw]' src="https://png.pngtree.com/thumb_back/fh260/background/20201012/pngtree-white-cloud-on-blue-sky-weather-background-image_410050.jpg" alt="" />
-        <div className='absolute top-[170px] left-[400px]'>
+        <img className='relative h-[80vh] w-[80vw]' src="https://github.com/alexkowsik/react-weather-app/blob/master/src/images/background.jpg?raw=true" alt="" />
+        <div className='absolute top-[120px] left-[400px]'>
           <input onChange={changeCity} value={city} className='w-[40vw] h-[5vh] rounded-lg' type="text" placeholder='Enter city Name' /></div>
-        <button className='absolute top-[175px] left-[880px] text-[25px] font-bold  hover:text-green-700'><CiSearch /></button>
+        {/* <button className='absolute top-[175px] left-[880px] text-[25px] font-bold  hover:text-green-700'><CiSearch /></button> */}
       </div>
+      {city &&
+        <div className=' h-[40vh] w-[42vw] absolute top-[200px] left-[380px] rounded-xl shadow-2xl'>
+          <img className='h-[200px] w-[160px] absolute left-[40px] top-[20px]' src="https://raw.githubusercontent.com/alexkowsik/react-weather-app/0db0670c49fb9c4be1ef85c95b01714def7ad3ab/src/images/01d.svg" alt="" />
+          <div>
+            <p>{Weather.name}</p>
+            <p>{Weather.main}</p>
+          </div>
+        </div>
+      
+     
+     
+      }
+
     </>
   )
 }
