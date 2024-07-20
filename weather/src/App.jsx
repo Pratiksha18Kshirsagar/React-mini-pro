b2970dd5ae0b0407bff558f4ad5c063477701407
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-07-20 07:14:55 +0530

    12

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -25,49 +25,47 @@ function App() {
         let data = await response.json()
         console.log(data)
         setWeather(data)
+
       } catch (error) {
         console.error(error);
         setWeather(null); // or setWeather({ name: 'Error', main: { temp: 'N/A' } }); to display an error message
       }
-      if (Weather.weather[0].main == "Clouds") {
-        image.current.src = "https://raw.githubusercontent.com/alexkowsik/react-weather-app/0db0670c49fb9c4be1ef85c95b01714def7ad3ab/src/images/03n.svg"
-      }
-      else if (Weather.weather[0].main == "Clear") {
-        image.current.src = "https://raw.githubusercontent.com/alexkowsik/react-weather-app/0db0670c49fb9c4be1ef85c95b01714def7ad3ab/src/images/01d.svg"
-      }
-      else if (Weather.weather[0].main == "Rain") {
-        image.current.src = "https://raw.githubusercontent.com/alexkowsik/react-weather-app/0db0670c49fb9c4be1ef85c95b01714def7ad3ab/src/images/10n.svg"
-      }
+
     }
     getWeather()
   }, [city])
 
-
+  useEffect(() => {
+    if (Weather) {
+      if (Weather.weather[0].main === "Clouds") {
+        image.current.src = "https://static.vecteezy.com/system/resources/thumbnails/018/931/690/small_2x/cute-cloud-cartoon-icon-png.png"
+      } else if (Weather.weather[0].main === "Clear") {
+        image.current.src = "https://icon2.cleanpng.com/20180308/kqw/kisspng-cartoon-sun-5aa187944ccf24.3627003215205354443146.jpg"
+      } else if (Weather.weather[0].main === "Rain") {
+        image.current.src = "https://png.pngtree.com/png-vector/20240126/ourmid/pngtree-cute-rain-cloud-png-image_11496082.png"
+      }
+    }
+  }, [Weather])
 
 
   return (
     <>
-      <div className=' h-[80vh] w-[80vw] bg-blue-100 '>
-        <img className='relative h-[80vh] w-[80vw]' src="https://github.com/alexkowsik/react-weather-app/blob/master/src/images/background.jpg?raw=true" alt="" />
-        <div className='absolute top-[120px] left-[400px]'>
-          <input onChange={changeCity} value={city} className='w-[40vw] h-[5vh] rounded-lg' type="text" placeholder='Enter city Name' /></div>
-        {/* <button className='absolute top-[175px] left-[880px] text-[25px] font-bold  hover:text-green-700'><CiSearch /></button> */}
-      </div>
-      {Weather &&
-        <div className=' h-[40vh] w-[42vw] absolute top-[200px] left-[380px] rounded-xl shadow-2xl'>
-          <img ref={image} className='h-[230px] w-[170px] absolute left-[40px] top-[20px]' src={null} alt="" />
-          <div className='absolute top-[60px] left-[230px] text-[25px]'>
-            <p className='font-extrabold  font-serif '>{Weather.name}</p>
-            <p>Temparature : {Weather.main.temp}°C</p>
-            <p>{Weather.weather[0].main}</p>
-            <p>{Weather.weather[0].description}</p>
-          </div>
+      <div className='image'>
+        <div className='flex justify-center items-center flex-col gap-[30px] mt-14'>
+          <input onChange={changeCity} value={city} className=' mt-32 w-[40vw] h-[5vh] rounded-lg' type="text" placeholder='Enter city Name' />
+          {Weather &&
+            <div className=' flex justify-center items-center h-[40vh] w-[42vw]  rounded-xl shadow-2xl max-md:flex max-md:flex-col max-md:items-center max-md:justify-center'>
+              <img ref={image} className='h-[230px] w-[170px] ' src={null} alt="" />
+              <div className=' text-[25px] text-center '>
+                <p className='font-extrabold  font-serif '>{Weather.name}</p>
+                <p>Temparature : {Weather.main.temp}°C</p>
+                <p>{Weather.weather[0].main}</p>
+                <p>{Weather.weather[0].description}</p>
+              </div>
+            </div>
+          }
         </div>
-
-
-
-      }
-
+      </div>
     </>
   )
 }
