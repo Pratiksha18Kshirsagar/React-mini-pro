e382cd24f0060ab5088adc6a54b4ebf2761498dd
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-07-21 08:20:10 +0530

    stopwatch

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useRef } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -25,17 +25,33 @@ function App() {
   }
 
   const stop = () => {
+    clearInterval(timeRef.current)
   }
 
   const reset = () => {
+    stop()
+    settime(0)
+
   }
 
   return (
     <>
-      <p>{(time/1000).toFixed(0)}</p>
-      <button onClick={start}><VscDebugStart /></button>
-      <button onClick={stop}><CiStopwatch /></button>
-      <button onClick={reset}><GrPowerReset /></button>
+    <div className='container bg-slate-100 h-[50vh] w-[40vw] text-black flex items-center justify-center flex-col rounded-2xl max-md:h-[85vh] max-lg:w-[85vw] '>
+      <div>
+        <p className='text-[30px]'>Stopwatch</p>
+        <p className='mt-10 text-center text-[30px]'>{(time / 1000).toFixed(0)}</p>
+        </div>
+
+      <div className='flex gap-14 p-11 max-md:flex max-md:flex-col '>
+        <button className='border border-white p-6 bg-green-300 text-black text-[25px] rounded-xl' 
+        onClick={start}><VscDebugStart /></button>
+        <button className='border border-white p-6 bg-yellow-200 text-black text-[25px] rounded-xl ' 
+        onClick={stop}><CiStopwatch /></button>
+        <button className='border border-white p-6 bg-red-300 text-black text-[25px] rounded-xl'
+         onClick={reset}><GrPowerReset /></button>
+    
+      </div>
+      </div>
     </>
   )
 }
